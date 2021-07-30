0ae4f4e1eda332c7472655c19815fcbd9a0421a9
rwanoux <erwan_lemaire@yahoo.fr>
2021-07-31 01:46:10 +0200

    clean

--- utils/functions.php
+++ utils/functions.php
@@ -1,29 +1,31 @@
 <?php
 //include ('./class/managers/MainManager.php');
 
-function getMatieresFromProf(Professeur $prof, MainManager $main){
+function getMatieresFromProf(Professeur $prof, MainManager $main)
+{
 
-$req=$main->getDb()->query('
-SELECT * FROM asso_prof_matiere WHERE code_prof_asso_prof_matiere="'.$prof->getCode_professeur().'"
+    $req = $main->getDb()->query('
+SELECT * FROM asso_prof_matiere WHERE code_prof_asso_prof_matiere="' . $prof->getCode_professeur() . '"
 ');
-$matieres = [];
-$dataAsso = $req->fetchAll(PDO::FETCH_ASSOC);
-foreach($dataAsso as $assoRow) {
-  
-    $req=$main->getDb()->query('
-    SELECT * FROM matieres WHERE code_matiere="'.$assoRow["code_matiere_asso_prof_matiere"].'"
+    $matieres = [];
+    $dataAsso = $req->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($dataAsso as $assoRow) {
+
+        $req = $main->getDb()->query('
+    SELECT * FROM matieres WHERE code_matiere="' . $assoRow["code_matiere_asso_prof_matiere"] . '"
     ');
-    while ($dataMat = $req->fetch(PDO::FETCH_ASSOC)) {
-        $matNom=$dataMat["nom_matiere"];
-        array_push($matieres, $matNom);
+        while ($dataMat = $req->fetch(PDO::FETCH_ASSOC)) {
+            $matNom = $dataMat["nom_matiere"];
+            array_push($matieres, $matNom);
+        }
     }
-}
-return $matieres;
+    return $matieres;
 }
 
-function getClassesFromProf(Professeur $prof, $man){
+function getClassesFromProf(Professeur $prof, $man)
+{
 
-   $clList=$man->getByAttributs(["code_prof_ref_classe"=>$prof->getCode_professeur()]);
-    
+    $clList = $man->getByAttributs(["code_prof_ref_classe" => $prof->getCode_professeur()]);
+
     return $clList;
-    }
+}
